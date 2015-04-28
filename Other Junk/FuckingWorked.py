a8b6d03909d23d66ea8304853ef83684bade5a8b
aknuck <arknw229@gmail.com>
2015-04-27 21:29:04 -0400

    payment no longer mandatory

---
+++ Other Junk/FuckingWorked.py
@@ -0,0 +1,9 @@
+print "Hey i think it worked!"
+print "nah brah"
+print "well shit"
+print "what now"
+print "hey can you see what i do in the console?"
+print "actually yes"
+print "hey buddy"
+while True:
+    print "worked"
