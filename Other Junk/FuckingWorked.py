b08e0b7b04054e4c4ca638256ec9deb6cc2b2ac3
Adam Kafka <adkafka@gmail.com>
2015-04-27 21:39:31 -0400

    Removed unnecessary files

--- Other Junk/FuckingWorked.py
+++
@@ -1,9 +0,0 @@
-print "Hey i think it worked!"
-print "nah brah"
-print "well shit"
-print "what now"
-print "hey can you see what i do in the console?"
-print "actually yes"
-print "hey buddy"
-while True:
-    print "worked"
