b0cc34865328940e9b2195d0fec3589167d7fbda
Alexander Hake <alexander.hake27@gmail.com>
2015-04-27 20:42:11 -0400

    Minor UI Change - Fixed Admin Pop Up

--- MainMenu.py
+++ MainMenu.py
@@ -20,13 +20,6 @@ class MainMenu(Frame):
 		btnWidth = 60
 		btnHeight = 2
 
-		#buffers
-		#buff = Label(self,text="Logged in as "+self.user)#,bg=self.color,padx=5)
-		#buff.grid(row=0,column=0,padx=10,pady=10,columnspan=5,sticky=('n', 's', 'e', 'w'))#,sticky='CENTER')
-
-		#buff2 = Label(self,text="               ")#,bg=self.color,padx=5)
-		#buff2.grid(row=0,column=0,rowspan=3,sticky='n')
-
 		self.frame = Frame (self)		
 		self.frame.grid (row=1, column=1)	
 
@@ -34,7 +27,7 @@ class MainMenu(Frame):
 		self.adminLoginButton = Button(self.frame, text="Admin", bg='light gray', width=10, command=lambda: self.adminLogin())
 		self.adminLoginButton.grid(row=0, column=1, sticky='e')
 
-		# create transaction
+		# create transaction 
 		self.createTransBtn = Button(self.frame, text="Create Transaction", bg='light gray', width=btnWidth, height=btnHeight, command=lambda: self.createTransaction())
 		self.createTransBtn.grid(row=1, column=0, columnspan=2, pady=10)
 
@@ -83,54 +76,52 @@ class MainMenu(Frame):
 		self.graphics.liftLayer("login")
 
 	def adminLogin(self,event=None):
-		try:
-			self.graphics.POS.logOut()
-		except:
-			print "already logged out probs"
-		print "admin login"
 
-		color = "#e8e8e8"
+		color = "#D1D1D1"
 
 		# Create progress bar pop-up window
 		self.adminWindow = Toplevel(self)
-		self.adminWindow.configure(bg=color)
+		#self.adminWindow.configure(bg=color)
+		self.adminWindow.color = "D1D1D1"
 		self.adminWindow.geometry("480x220+550+150")
 
-		#buffers
-		buff = Label(self.adminWindow,text="               ")#,bg=self.color,padx=5)
-		buff.grid(row=1,column=0,sticky='w')	
-
-		buff2 = Label(self.adminWindow,text="               ")#,bg=self.color,padx=5)
-		buff2.grid(row=2,column=1,sticky='n')
+		self.adminWindow.adminFrame = Frame (self.adminWindow)		
+		self.adminWindow.adminFrame.grid (row=1, column=1)
 
 		# Enter Username
-		usernameLabel = Label(self.adminWindow, text="username")#, bg=self.color,padx=5)
+		usernameLabel = Label(self.adminWindow.adminFrame, text="Username")#, bg=self.color,padx=5)
 		#usernameLabel = Label(self, text="username", bg=self.color,padx=5)
-		usernameLabel.grid(row=3,column=1,sticky='s')
-		usernameField = Entry(self.adminWindow, width=40)#bd =0, width=40)
+		usernameLabel.grid(row=0,column=0,sticky='s')
+		usernameField = Entry(self.adminWindow.adminFrame, width=40)#bd =0, width=40)
 		#usernameField = Entry(self, width=40,bd =1)
-		usernameField.grid(row=4,column=1,sticky='n')
+		usernameField.grid(row=1,column=0,sticky='n')
 		usernameField.bind("<Return>",(lambda event: self.login(usernameField,passwordField)))
+		usernameField.focus()
 
 		# Enter Password
-		passwordLabel = Label(self.adminWindow, text="password")#, bg=self.color,padx=5)
+		passwordLabel = Label(self.adminWindow.adminFrame, text="Password")#, bg=self.color,padx=5)
 		#passwordLabel = Label(self, text="password", bg=self.color,padx=5)
-		passwordLabel.grid(row=5,column=1,sticky='s')
-		passwordField = Entry(self.adminWindow, show="*", width=40)#bd =0, width=40)
+		passwordLabel.grid(row=2,column=0,sticky='s')
+		passwordField = Entry(self.adminWindow.adminFrame, show="*", width=40)#bd =0, width=40)
 		#passwordField = Entry(self, show="*", width=40,bd =1)
-		passwordField.grid(row=6,column=1,sticky='n')
+		passwordField.grid(row=3,column=0,sticky='n')
 		passwordField.bind("<Return>",(lambda event: self.login(usernameField,passwordField)))
 
 		#output
-		self.adminOutputLabel = Label(self.adminWindow,text="",style="Red.TLabel")#,bg=self.color,padx=5)
-		self.adminOutputLabel.grid(row=7,column=1,sticky='s')
+		self.adminOutputLabel = Label(self.adminWindow.adminFrame,text="",style="Red.TLabel")#,bg=self.color,padx=5)
+		self.adminOutputLabel.grid(row=4,column=0,sticky='s')
 
 		# Attempt to login
-		loginButton = Button(self.adminWindow, text="Enter", width=15, style='green/black.TButton', command=lambda: self.login(usernameField, passwordField))
+		loginButton = Button(self.adminWindow.adminFrame, text="Login", width=15, bg="light gray", command=lambda: self.login(usernameField, passwordField))
 		#loginButton = Button(self, text="Enter", width=15, command=lambda: self.login(usernameField, passwordField))
-		loginButton.grid(row=8,column=1,pady=10)
+		loginButton.grid(row=5,column=0,pady=10)
 
-	def login(self,username,password,event=None):
+		self.adminWindow.grid_columnconfigure (0, weight=1)
+		self.adminWindow.grid_columnconfigure (2, weight=1)
+		self.adminWindow.grid_rowconfigure 	  (0, weight=1)
+		self.adminWindow.grid_rowconfigure    (2, weight=1)
+
+	def login(self, username, password, event=None):
 		if self.graphics.POS.login(username.get(),password.get()) == True:
 			print "logged in!"
 			self.adminWindow.destroy()
